49cf226e1dc92c2a25ed6e6f80a03918aa0a9f00
Giuseppe Cirrone <cirrone@lns.infn.it>
2022-05-29 13:30:23 +0200

    adding the proton source from PALS and reconstructing the spectra

--- LaserProtonMicrodosimetry/LaserProtonMicrodosimetry.m
+++ LaserProtonMicrodosimetry/LaserProtonMicrodosimetry.m
@@ -1,3 +1,19 @@
-Scorer = ReadPHSPFile('Scorer_Pablo.txt')
+clear all
+close all
+Scorer = ReadPHSPFile_2('Scorer_Pablo.phsp');
 
-hist(Scorer.VarName6)
+% Particle arrival time
+%
+ParticleArrivalTime = Scorer.VarName10;
+
+% Particle kinetic energy at the detector surface
+%
+ParticleKineticEnergy = Scorer.VarName6;
+
+%[countsE_kin, binE_kin] = hist(Scorer.VarName6, 300);
+
+
+%[count_time, bin_time] = hist(Scorer.VarName10, 300);
+
+plot(ArrivalTime, ParticleKineticEnergy);
+
